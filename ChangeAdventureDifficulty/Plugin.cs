5ab539ab1a87aab1b9b867f7fb6471c049c82de6
Deathblood <deathbloodjr@gmail.com>
2025-03-12 14:06:43 -0500

    SaveProfileManager adjustments
    And updated version and README

--- ChangeAdventureDifficulty/Plugin.cs
+++ ChangeAdventureDifficulty/Plugin.cs
@@ -7,6 +7,8 @@ using BepInEx.Configuration;
 using ChangeAdventureDifficulty.Plugins;
 using UnityEngine;
 using System.Collections;
+using SaveProfileManager.Plugins;
+using System.Reflection;
 
 namespace ChangeAdventureDifficulty
 {
@@ -31,20 +33,26 @@ namespace ChangeAdventureDifficulty
 
             Log = base.Log;
 
-            SetupConfig();
+            SetupConfig(Config, Path.Combine("BepInEx", "data", ModName));
             SetupHarmony();
+
+            var isSaveManagerLoaded = IsSaveManagerLoaded();
+            if (isSaveManagerLoaded)
+            {
+                AddToSaveManager();
+            }
         }
 
-        private void SetupConfig()
+        private void SetupConfig(ConfigFile config, string saveFolder, bool isSaveManager = false)
         {
             var dataFolder = Path.Combine("BepInEx", "data", ModName);
 
-            ConfigEnabled = Config.Bind("General",
+            ConfigEnabled = config.Bind("General",
                 "Enabled",
                 true,
                 "Enables the mod.");
 
-            ConfigSelectedDifficulty = Config.Bind("General",
+            ConfigSelectedDifficulty = config.Bind("General",
                 "SelectedDifficulty",
                 "Oni",
                 "Difficulty to set any story mode songs to. (Easy, Normal, Hard, Oni, Ura)");
@@ -55,29 +63,35 @@ namespace ChangeAdventureDifficulty
             // Patch methods
             _harmony = new Harmony(MyPluginInfo.PLUGIN_GUID);
 
-            if (ConfigEnabled.Value)
+            LoadPlugin(ConfigEnabled.Value);
+        }
+
+        public static void LoadPlugin(bool enabled)
+        {
+            if (enabled)
             {
                 bool result = true;
                 // If any PatchFile fails, result will become false
-                result &= PatchFile(typeof(ChangeAdventureDifficultyPatch));
-                result &= PatchFile(typeof(SaveScenarioPlay));
+                result &= Instance.PatchFile(typeof(ChangeAdventureDifficultyPatch));
+                result &= Instance.PatchFile(typeof(SaveScenarioPlay));
                 if (result)
                 {
-                    Log.LogInfo($"Plugin {MyPluginInfo.PLUGIN_NAME} is loaded!");
+                    Logger.Log($"Plugin {MyPluginInfo.PLUGIN_NAME} is loaded!");
                 }
                 else
                 {
-                    Log.LogError($"Plugin {MyPluginInfo.PLUGIN_GUID} failed to load.");
+                    Logger.Log($"Plugin {MyPluginInfo.PLUGIN_GUID} failed to load.", LogType.Error);
                     // Unload this instance of Harmony
                     // I hope this works the way I think it does
-                    _harmony.UnpatchSelf();
+                    Instance._harmony.UnpatchSelf();
                 }
             }
             else
             {
-                Log.LogInfo($"Plugin {MyPluginInfo.PLUGIN_NAME} is disabled.");
+                Logger.Log($"Plugin {MyPluginInfo.PLUGIN_NAME} is disabled.");
             }
         }
+
 
         private bool PatchFile(Type type)
         {
@@ -89,14 +103,54 @@ namespace ChangeAdventureDifficulty
             {
                 _harmony.PatchAll(type);
 #if DEBUG
-                Log.LogInfo("File patched: " + type.FullName);
+                Logger.Log("File patched: " + type.FullName);
 #endif
                 return true;
             }
             catch (Exception e)
             {
-                Log.LogInfo("Failed to patch file: " + type.FullName);
-                Log.LogInfo(e.Message);
+                Logger.Log("Failed to patch file: " + type.FullName);
+                Logger.Log(e.Message);
+                return false;
+            }
+        }
+
+        public static void UnloadPlugin()
+        {
+            Instance._harmony.UnpatchSelf();
+            Logger.Log($"Plugin {MyPluginInfo.PLUGIN_NAME} has been unpatched.");
+        }
+
+        public static void ReloadPlugin()
+        {
+            // Reloading will always be completely different per mod
+            // You'll want to reload any config file or save data that may be specific per profile
+            // If there's nothing to reload, don't put anything here, and keep it commented in AddToSaveManager
+            //SwapSongLanguagesPatch.InitializeOverrideLanguages();
+            //TaikoSingletonMonoBehaviour<CommonObjects>.Instance.MyDataManager.MusicData.Reload();
+        }
+
+        public void AddToSaveManager()
+        {
+            // Add SaveDataManager dll path to your csproj.user file
+            // https://github.com/Deathbloodjr/RF.SaveProfileManager
+            var plugin = new PluginSaveDataInterface(MyPluginInfo.PLUGIN_GUID);
+            plugin.AssignLoadFunction(LoadPlugin);
+            plugin.AssignUnloadFunction(UnloadPlugin);
+            //plugin.AssignReloadSaveFunction(ReloadPlugin);
+            plugin.AssignConfigSetupFunction(SetupConfig);
+            plugin.AddToManager();
+        }
+
+        private bool IsSaveManagerLoaded()
+        {
+            try
+            {
+                Assembly loadedAssembly = Assembly.Load("com.DB.RF.SaveProfileManager");
+                return loadedAssembly != null;
+            }
+            catch
+            {
                 return false;
             }
         }
--- README.md
+++ README.md
@@ -4,6 +4,8 @@
 
  To use it, set the SelectedDifficulty config value to whatever difficulty you want.\
  With Ura, it will only select Ura for songs that have an Ura chart, otherwise it will pick Oni.
+  
+ <a href="https://shorturl.at/1TERY"> <img src="Resources/InstallButton.png" alt="One-click Install using the Taiko Mod Manager" width="256"/> </a>
  
 # Requirements
  Visual Studio 2022 or newer\
@@ -12,13 +14,16 @@
  
 
 # Build
- Install [BepInEx be 697](https://builds.bepinex.dev/projects/bepinex_be) or [BepInEx 6.0.0-pre.2](https://github.com/BepInEx/BepInEx/releases/tag/v6.0.0-pre.2) into your Rhythm Festival directory and launch the game.\
+ Install [BepInEx 6.0.0-pre.2](https://github.com/BepInEx/BepInEx/releases/tag/v6.0.0-pre.2) into your Rhythm Festival directory and launch the game.\
  This will generate all the dummy dlls in the interop folder that will be used as references.\
  Make sure you install the Unity.IL2CPP-win-x64 version.\
  Newer versions of BepInEx could have breaking API changes until the first stable v6 release, so those are not recommended at this time.
  
  Attempt to build the project, or copy the .csproj.user file from the Resources file to the same directory as the .csproj file.\
- Edit the .csproj.user file and place your Rhythm Festival file location in the "GameDir" variable.
+ Edit the .csproj.user file and place your Rhythm Festival file location in the "GameDir" variable.\
+ Download or build the [SaveProfileManager](https://github.com/Deathbloodjr/RF.SaveProfileManager) mod, and place that dll full path in SaveProfileManagerPath.
+
+Add BepInEx as a nuget package source (https://nuget.bepinex.dev/v3/index.json)
 
 # Links 
  [My Other Rhythm Festival Mods](https://docs.google.com/spreadsheets/d/1xY_WANKpkE-bKQwPG4UApcrJUG5trrNrbycJQSOia0c)\
